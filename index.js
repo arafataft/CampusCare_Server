e20953f3ab066f339056a252098de67d5ed2ca33
arafataft <arafatcd@gmail.com>
2023-07-24 21:27:16 +0600

    vercel

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ async function run() {
     const CollegeDetails = client.db('campusCareDB').collection('colleges');
     const ResearchPaper = client.db('campusCareDB').collection('papers');
     const admissionCollection = client.db('campusCareDB').collection('admissions');
+    const reviewCollection = client.db('campusCareDB').collection('reviews');
 
 
     app.get('/colleges', async (req, res) => {
@@ -88,7 +89,7 @@ async function run() {
         }
     
         // Check if the email address is already admitted to the college
-        const admissionCollection = client.db('campusCareDB').collection('admissions');
+       
         const existingAdmission = await admissionCollection.findOne({
           candidateEmail: admissionData.candidateEmail
         });
@@ -109,6 +110,79 @@ async function run() {
       }
     });
     
+
+
+
+// Route to get college details for the user based on their email
+app.get('/mycollege', async (req, res) => {
+  try {
+    const userEmail = req.query.email; // Assuming you pass the user's email as a query parameter
+    console.log(userEmail);
+    // Fetch the admission details for the user from the admissions collection
+    const userAdmission = await admissionCollection.findOne({ candidateEmail: userEmail });
+
+    if (!userAdmission) {
+      return res.status(404).json({ error: 'Admission not found for this user.' });
+    }
+
+    // If the user's admission is found, you can send the college details back to the frontend
+    const collegeDetails = {
+      name: userAdmission.candidateName,
+      collegeName:userAdmission.collegeName,
+      address: userAdmission.address,
+      phone: userAdmission.candidatePhone,
+      email: userAdmission.candidateEmail
+    };
+
+    res.json(collegeDetails);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+
+
+// Import the required modules and setup the app...
+
+// Route to post the review to the backend
+app.post('/reviews', async (req, res) => {
+  try {
+    const { collegeName, reviewRating, reviewText,name,email } = req.body;
+
+  
+
+    const reviewData = {
+      name,
+      collegeName,
+      rating: parseInt(reviewRating),
+      text: reviewText,
+      email 
+    };
+
+    
+    await reviewCollection.insertOne(reviewData);
+
+    res.status(200).json({ message: 'Review submitted successfully!' });
+  } catch (error) {
+    console.error('Error submitting review:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+
+// Route to get the reviews from the backend
+app.get('/reviews', async (req, res) => {
+  try {
+    const reviews = await reviewCollection.find().toArray();
+    res.status(200).json(reviews);
+  } catch (error) {
+    console.error('Error fetching reviews:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 
 
